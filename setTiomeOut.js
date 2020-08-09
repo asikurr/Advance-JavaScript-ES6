7e03934b4cd51a21864e200a57bde888dd4535ae
Asikur <asikurrahman7@gmail.com>
2020-08-09 16:21:22 +0600

    async await this time setimeout.

---
+++ setTiomeOut.js
@@ -0,0 +1,11 @@
+function doSomething() {
+    console.log(1111);    
+}
+
+console.log(2222);   
+console.log(3333);   
+console.log(4444);   
+setTimeout( () => {
+     console.log("i am lazy.");
+} , 2000)
+console.log(5555);   
